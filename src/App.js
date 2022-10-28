de416e890d74a5aaaa4338dc3e2988ac6d746444
Aryan <aryan16072001236809@gmail.com>
2022-10-28 16:19:53 +0530

    Creating Dice app using Component,Props and Working with the setState

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Die from './Die';
+import RollDice from './RollDice';
+
+function App() {
+  return (
+    <div className="App">
+        <RollDice/>
+    </div>
+  );
+}
+
+export default App;
